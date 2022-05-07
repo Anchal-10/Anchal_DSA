2db61b22cb05315d36077c51d388a863900224e9
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-05-08 00:50:48 +0530

    Time: 37 ms (52.11%), Space: 61.2 MB (86.74%) - LeetHub

--- 456-132-pattern/456-132-pattern.java
+++ 456-132-pattern/456-132-pattern.java
@@ -1,22 +1,19 @@
 class Solution {
     public boolean find132pattern(int[] nums) {
-        int[] min = new int[nums.length];
-        min[0] = nums[0];
-        
-        for(int i=1;i<nums.length;i++)
-            min[i] = Math.min(min[i-1],nums[i]);
-        
+        int secondmax=Integer.MIN_VALUE;
         Stack<Integer> st = new Stack<>();
         
         for(int j=nums.length-1;j>=0;j--)
         {
-            while(st.size()>0 && st.peek() <= min[j])
-                st.pop();
-            if(st.size() > 0 && st.peek() < nums[j])
-                return true;
+            if(secondmax>nums[j]){
+return true;
+            }
+            while(st.size()>0 && st.peek() <nums[j]){
+            secondmax=Math.max(secondmax,  st.pop());
+            }
             st.push(nums[j]);
         }
-        
+    
         return false;
     }
 }
