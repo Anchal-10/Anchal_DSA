5711e52549ad7dbe5a7b31c1af480101602f5549
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-05-12 17:32:40 +0530

    Time: 1 ms (98.46%), Space: 42.4 MB (92.74%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> al=new ArrayList<>();
+        Arrays.sort(nums);
+        solve(al,new ArrayList<>(),0,nums);
+        return al;
+    }
+    
+    public void solve(List<List<Integer>> res,List<Integer> temp,int ind,int[]a){
+        res.add(new ArrayList<>(temp));
+        for(int i=0;i<a.length;i++){
+                             if(i==ind|| i>ind&& a[i]!=a[i-1]){
+
+            temp.add(a[i]);
+            solve(res,temp,i+1,a);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
+}
