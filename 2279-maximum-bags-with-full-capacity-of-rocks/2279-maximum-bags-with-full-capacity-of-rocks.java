e1d47ca3dd4b2e283859e61220d827d40cfa7e1c
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-05-23 23:52:22 +0530

    Time: 21 ms (85.71%), Space: 90.2 MB (14.29%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
@@ -1,24 +1,19 @@
 class Solution {
     public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
-        int total=additionalRocks;
-        int c=0;
-        int diff[] = new int[capacity.length];
-        
-        for(int i=0;i<capacity.length;i++){
+        int diff[]=new int[capacity.length];
+        for(int i=0;i< capacity.length;i++){
             diff[i]=capacity[i]-rocks[i];
         }
-        //sort the array 
-        Arrays.sort(diff);
-        
-        // to fill most of the bags, it will be benificial for us to choose the one with the lowest difference
-         for(int i=0;i<capacity.length;i++){
-            if(total>=diff[i]){
-                total-=diff[i];
-                c++;
-            }
+                Arrays.sort(diff);
+
+        int ans=0;
+        for(int i: diff){
+            if(i<=additionalRocks){
+               additionalRocks= additionalRocks-i;
+            
+            ans++;
         }
-        return c;
-        
-        
+        }
+        return ans;
     }
 }
