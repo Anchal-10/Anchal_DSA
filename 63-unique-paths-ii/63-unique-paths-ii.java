393c35106c025d24e421ce05c65254071c2a2a07
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-07-02 16:04:46 +0530

    Time: 1 ms (56.30%), Space: 42.9 MB (12.79%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -5,28 +5,29 @@ class Solution {
         int n=obstacleGrid[0].length;
 int dp[][]=new int[m][n];
         for(int a[]:dp){
-            Arrays.fill(a,-1);
+        Arrays.fill(a,-1);
         }
-      int c=  solve(obstacleGrid,m-1,n-1,dp);
-        return c;
-    }
+    int ans=solve(obstacleGrid,m-1,n-1,dp);
+        return ans;
+}
+    
     public int solve(int a[][],int m,int n,int[][]dp){
-        if(m<0||n<0){
-            return 0;
+                if(n<0|| m<0){
+return 0;
         }
+
                 if(a[m][n]==1){
-            return 0;
+return 0;
         }
-                if(m==0&& n==0){
-            return 1;
+
+        if(n==0&& m==0){
+return 1;
         }
         if(dp[m][n]!=-1){
 return dp[m][n];
         }
-
-
         int left=solve(a,m-1,n,dp);
         int right=solve(a,m,n-1,dp);
-        return  dp[m][n]=left+right;
+        return dp[m][n]=left+right;
     }
 }
