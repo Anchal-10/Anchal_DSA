f9f555ba2b05949ac1769a69bbb5f4c511b9d11a
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-07-13 14:19:01 +0530

    Time: 802 ms (5.46%), Space: 242.5 MB (5.06%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,22 +1,27 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int dp[]=new int[nums.length];
-        dp[0]=1;
-        for(int i=1;i<nums.length;i++){
-            int max=0;
-            for(int j=0;j<i;j++){
-                if(nums[j]<nums[i]){
-                    if(dp[j]>=max){
-                        max=dp[j];
-                    }
-                }
-            }
-            dp[i]=max+1;
-        }
-        int m=0;
-        for(int i:dp){
-m=Math.max(i,m);
-        }
+        int dp[][]=new int[nums.length][nums.length+1];
+        for(int[]a: dp){
+Arrays.fill(a,-1);
+    }
+    
+int m=helper(nums,0,-1,dp);
         return m;
+    }
+    public int helper(int []a,int ind,int prev_ind,int[][]dp){
+        
+        if(ind==a.length){
+return 0;
+        }
+        if(dp[ind][prev_ind+1]!=-1){
+return dp[ind][prev_ind+1];
+        }
+        
+        int not_take=helper(a,ind+1,prev_ind,dp);
+        int take=Integer.MIN_VALUE;
+        if(prev_ind==-1|| a[ind]>a[prev_ind]){
+            take=1+helper(a,ind+1,ind,dp);
+        }
+        return  dp[ind][prev_ind+1]=Math.max(take,not_take);
     }
 }
