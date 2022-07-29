5778bba31bd631280f29d172bd2b8116b9ee4592
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-07-29 22:10:49 +0530

    Time: 1 ms (63.16%), Space: 42.4 MB (38.95%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -14,31 +14,25 @@
  * }
  */
 class Solution {
-    boolean flag=true;
     public boolean isSymmetric(TreeNode root) {
         if(root==null){
+            return false;
+        }
+      return  helper(root.left,root.right);
+        
+    }
+    public boolean helper(TreeNode a,TreeNode b){
+        if(a==null&& b==null){
 return true;
         }
-        
-        
-    helper(root.left,root.right);
-        return flag;
-    }
-    public void helper(TreeNode root1,TreeNode root2){
-if(root1==null&& root2==null){
-return;
-}
-        if(root1==null&& root2!=null|| root1!=null&&root2==null){
-flag=false;
-            return;
-        }
-        if(root1.val!=root2.val){
-            flag=false;
-        }
-        helper(root1.left,root2.right);
-                helper(root1.right,root2.left);
+                if(a==null|| b==null){
+return false;
 
-    
-    
+    }
+                if(a.val!=b.val){
+return false;
+
+}
+    return helper(a.left,b.right)&& helper(a.right,b.left);
     }
 }
