3339e23c95aa84499bc75b2cd039a4c61554b89b
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-07-29 11:26:57 +0530

    Time: 23 ms (96.72%), Space: 46 MB (99.35%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -3,34 +3,33 @@ class Solution {
         List<List<Integer>> al=new ArrayList<>();
         Arrays.sort(nums);
         for(int i=0;i<nums.length-2;i++){
-            if(i==0||(i>0)&& nums[i]!=nums[i-1]){
-                int sum=0-nums[i];
+            if(i==0|| i>0&& nums[i-1]!=nums[i]){
+                int temp=-nums[i];
                 int j=i+1;
                 int k=nums.length-1;
                 while(j<k){
-                    if(nums[j]+nums[k]==sum){
+                    if(nums[j]+nums[k]==temp){
                         al.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                        while(j<nums.length-1&&nums[j]==nums[j+1]){
+                        while(j<nums.length-1&&nums[j+1]==nums[j]){
                             j++;
                         }
-                                                while(k>0&&nums[k]==nums[k-1]){
+                                                while(k>0&&nums[k-1]==nums[k]){
                             k--;
                         }
+
+                        
                         j++;
                         k--;
                     }
-                    else if(k>0&&nums[j]+nums[k]<sum){
-j++;
+                    else if(nums[j]+nums[k]<temp){
+                        j++;
                     }
-                                        else if(nums[j]+nums[k]>sum){
-k--;
-                    }
-
-                        
-
+                    else{
+                        k--;
                     }
                 }
             }
-        return al;
         }
+        return al;
     }
+}
