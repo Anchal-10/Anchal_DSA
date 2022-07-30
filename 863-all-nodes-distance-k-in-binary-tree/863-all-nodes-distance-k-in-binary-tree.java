350b7a945d0ad15af0cf14d10ffa1a43cc3d60ae
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-07-30 12:22:51 +0530

    Time: 37 ms (5.49%), Space: 45.5 MB (5.24%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
@@ -8,76 +8,64 @@
  * }
  */
 class Solution {
+    
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        List<Integer> al=new ArrayList<>();
-HashMap<TreeNode,TreeNode > map=new HashMap<>();
-        helper(root,map);
+        List<Integer> res=new ArrayList<>();
+        HashMap<TreeNode,TreeNode> map=new HashMap<>();
+        mappingParent(root,map);
+        HashMap<TreeNode,Boolean> visited=new HashMap<>();
         Queue<TreeNode> queue=new LinkedList<>();
-        HashMap<TreeNode,Boolean> kmap=new HashMap<>();
         queue.offer(target);
-        kmap.put(target,true);
-        int c=0;
+        visited.put(target,true);
+        int distance=0;
         while(!queue.isEmpty()){
+            if(distance==k){
+                break;
+            }
+                        distance++;
+
             int size=queue.size();
-            if(c==k){
-break;
-            }
-            c++;
             for(int i=1;i<=size;i++){
-            TreeNode temp=queue.poll();
-            if(temp.left!=null&& kmap.get(temp.left)==null){
-queue.offer(temp.left);
-            kmap.put(temp.left,true);
-            }
-                            if(temp.right!=null&& kmap.get(temp.right)==null){
-queue.offer(temp.right);
-            kmap.put(temp.right,true);
-            }
-            if(map.get(temp)!=null&& kmap.get(map.get(temp))==null){
-queue.offer(map.get(temp));
-            kmap.put(map.get(temp),true);
-            }
+                TreeNode temp=queue.poll();
+                if(temp.left!=null&& visited.get(temp.left)==null){
+                    queue.offer(temp.left);
+                    visited.put(temp.left,true);
+                }
+                                if(temp.right!=null&& visited.get(temp.right)==null){
+                    queue.offer(temp.right);
+                                                        visited.put(temp.right,true);
+
+                }
+                if(map.get(temp)!=null&& visited.get(map.get(temp))==null){
+                    queue.offer(map.get(temp));
+                                        visited.put(map.get(temp),true);
+
+                }
 
             }
         }
         while(!queue.isEmpty()){
-            al.add(queue.poll().val);
+            res.add(queue.poll().val);
         }
-        return al;
+    return res;
     }
-public void helper(TreeNode root,HashMap<TreeNode,TreeNode> map){
-    if(root==null){
-return;
-    }
-    Queue<TreeNode> queue=new LinkedList<>();
-    queue.add(root);
-    while(!queue.isEmpty()){
-        int size=queue.size();
-        for(int i=1;i<=size;i++){
+    public void mappingParent(TreeNode root,HashMap<TreeNode,TreeNode> map){
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
             TreeNode temp=queue.poll();
             if(temp.left!=null){
-                queue.offer(temp.left);
+                queue.add(temp.left);
                 map.put(temp.left,temp);
-}
+            }
                         if(temp.right!=null){
-                queue.offer(temp.right);
-                map.put(temp.right,temp);
-}
+                queue.add(temp.right);
+                                            map.put(temp.right,temp);
+
+            }
         }
+        System.out.print(map);
     }
 }
-}
 
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
