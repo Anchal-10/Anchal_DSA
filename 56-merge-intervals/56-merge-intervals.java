d7f0ec99ec2973289453aca427d8ab0578c7e2f7
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-08-03 16:11:03 +0530

    Time: 20 ms (15.68%), Space: 55.6 MB (26.57%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,34 +1,32 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)-> Integer.compare(a[0],b[0]));
-    Stack<int[]> stack=new Stack<>();
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        Stack<int[]> stack=new Stack<>();
         stack.push(intervals[0]);
         for(int i=1;i<intervals.length;i++){
-            int m=intervals[i][0];
-            int n=intervals[i][1];
-            int poparr[]=stack.pop();
-            int c=poparr[0];
-            int d=poparr[1];
-            int endmax=Math.max(d,n);
-            if(d>=m){
-                int merge[]=new int[]{c,endmax};
-                stack.push(merge);
+            int start2=intervals[i][0];
+            int end2=intervals[i][1];
+            int []poparr=stack.pop();
+            int start1= poparr[0];
+            int end1= poparr[1];
+            if(end1>=start2){
+              int  endmax=Math.max(end1,end2);
+                int temp[]=new int[]{start1,endmax};
+                stack.push(temp);
+                
             }
             else{
+                
                 stack.push(poparr);
-                                stack.push(intervals[i]);
-
+                stack.push(intervals[i]);
             }
         }
-        
-    
-    int a[][]=new int[stack.size()][2];
-    int i=stack.size()-1;
-    while(!stack.isEmpty()){
-int t[]=stack.pop();
-     a[i]=t;
-        i--;
-    }
-        return a;
-    }
+        int res[][]=new int[stack.size()][2];
+        int j=0;
+        while(!stack.isEmpty()){
+res[j++]=stack.pop();
+        }
+
+     return res;       
 }
+    }
