b0b15f870203423adc19a0690bb99f3c79f27040
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-08-03 23:49:46 +0530

    Time: 425 ms (25.74%), Space: 67.8 MB (29.41%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,42 +1,41 @@
 class Solution {
     public String longestPalindrome(String s) {
-boolean dp[][]=new boolean[s.length()][s.length()];
-            String rev="";
-    int l=-1;
-        int m=-1;
-        for(int gap=0;gap<s.length();gap++){
-for(int i=0, j=i+gap;j<s.length();j++,i++){
-    
-    if(gap==0){
+        boolean dp[][]=new boolean[s.length()][s.length()];
+        int l1=-1;
+        int l2=-1;
+        for(int gap=0; gap<s.length();gap++){
+            for(int i=0,j=i+gap;j<s.length();j++,i++){
+                if(gap==0){
 dp[i][j]=true;
-    }
-    else if(gap==1){
-if(s.charAt(i)==s.charAt(j)){
-    
-    dp[i][j]=true;
-}
-        else{
-            dp[i][j]=false;
-        }
-        
-        }
-    else if(gap>1){
-    if(s.charAt(i)==s.charAt(j)&&dp[i+1][j-1]==true){
-        dp[i][j]=true;
+                }
+                else                 if(gap==1){
+                    if(s.charAt(i)==s.charAt(j)){
+dp[i][j]=true;
+                }
+                    else{
+                    dp[i][j]=false;
+                    }
 
-    }
-        else{
-            dp[i][j]=false;
-        }
-}
-    if(dp[i][j]){
-        l=i;
-        m=j;
-    }
             }
-            
-        }
-             return s.substring(l,m+1);
+                else {
+                    
+                                        if(s.charAt(i)==s.charAt(j)&& dp[i+1][j-1]==true){
+                                            dp[i][j]=true;
 
+                    
+                }
+                    else{
+                        dp[i][j]=false;
+                    }
+        }
+                
+                if(dp[i][j]){
+                    l1=i;
+                    l2=j;
+                    
+                }
+            }
+        }
+        return s.substring(l1,l2+1);
     }
 }
