e8aed64efee4cfbde986eb8b3c22b65aefc98925
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-08-04 11:06:13 +0530

    Time: 259 ms (35.33%), Space: 45.2 MB (52.40%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,41 +1,40 @@
 class Solution {
     public String longestPalindrome(String s) {
-        boolean dp[][]=new boolean[s.length()][s.length()];
+        int n=s.length();
+        boolean [][]dp=new boolean[n][n];
         int l1=-1;
         int l2=-1;
-        for(int gap=0; gap<s.length();gap++){
-            for(int i=0,j=i+gap;j<s.length();j++,i++){
-                if(gap==0){
-dp[i][j]=true;
-                }
-                else                 if(gap==1){
-                    if(s.charAt(i)==s.charAt(j)){
-dp[i][j]=true;
-                }
-                    else{
-                    dp[i][j]=false;
-                    }
+        for(int gap=0;gap<s.length();gap++){
+                    for(int i=0,j=i+gap;j<s.length();j++,i++){
+                        if(gap==0){
+                            dp[i][j]=true;
+                        }
+                        else if(gap==1){
+                            if(s.charAt(i)==s.charAt(j)){
+                                dp[i][j]=true;
+                            }
+                            else{
+                                dp[i][j]=false;
+                            }
+                        }
+                        else if(gap>1){
+                                                        if(s.charAt(i)==s.charAt(j)&& dp[i+1][j-1]==true){
+                                dp[i][j]=true;
+                            }
+                            else{
+                                dp[i][j]=false;
+                            }
+                        }
+                        if(dp[i][j]){
+                            l1=i;
+                            l2=j;
+                        }
 
-            }
-                else {
-                    
-                                        if(s.charAt(i)==s.charAt(j)&& dp[i+1][j-1]==true){
-                                            dp[i][j]=true;
+                            
+                        }
+        }
+            return s.substring(l1,l2+1);
 
-                    
-                }
-                    else{
-                        dp[i][j]=false;
-                    }
-        }
-                
-                if(dp[i][j]){
-                    l1=i;
-                    l2=j;
-                    
-                }
-            }
-        }
-        return s.substring(l1,l2+1);
+        
     }
 }
