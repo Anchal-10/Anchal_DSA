d33a26a5febe1260fe943f23f78ad250768f3b24
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-08-09 12:35:30 +0530

    Time: 6 ms (24.19%), Space: 48.3 MB (17.24%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,30 +1,32 @@
 class Solution {
-    int count=0;
-    
     public int maxAreaOfIsland(int[][] grid) {
-        int max=Integer.MIN_VALUE;
-        boolean [][]visited=new boolean[grid.length][grid[0].length];
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1&& visited[i][j]==false){
-                    findArea(grid,visited,i,j);
-                    max=Math.max(count,max);
-                    count=0;
+        int m=grid.length;
+        int n=grid[0].length;
+        int max=0;
+        boolean visited[][]=new boolean[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1 && visited[i][j]==false){
+                    List<Integer> al=new ArrayList<>();
+                    helper(grid,i,j,m,n,visited,al);
+                    max=Math.max(al.size(),max);
                 }
             }
         }
-        return max==Integer.MIN_VALUE?0:max;
+        return max;
     }
-    public void findArea(int a[][],boolean[][]visited,int i,int j){
-        if(i<0|| i>=a.length|| j<0|| j>=a[0].length|| visited[i][j]==true|| a[i][j]==0){
-            return ;
+    
+    public void helper(int [][]a,int i,int j,int m,int n,boolean[][]visited,List<Integer> al){
+        if(i<0|| j<0|| i>=m|| j>=n || visited[i][j]==true|| a[i][j]==0){
+            return;
         }
-        count++;
+        al.add(a[i][j]);
         visited[i][j]=true;
-        findArea(a,visited,i+1,j);
-                findArea(a,visited,i-1,j);
-        findArea(a,visited,i,j-1);
-        findArea(a,visited,i,j+1);
+        helper(a,i+1,j,m,n,visited,al);
+        helper(a,i-1,j,m,n,visited,al);
+        helper(a,i,j-1,m,n,visited,al);
+        helper(a,i,j+1,m,n,visited,al);
 
+        
     }
 }
