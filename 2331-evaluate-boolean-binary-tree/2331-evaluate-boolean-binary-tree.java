11fdca9141f0681d2cf04bb1952a139dc31eb8d0
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-08-17 23:20:53 +0530

    Time: 1 ms (60.26%), Space: 46.6 MB (37.39%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.java
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.java
@@ -14,12 +14,22 @@
  * }
  */
 class Solution {
-        
     public boolean evaluateTree(TreeNode root) {
-        if(root.val == 0) return false;
-        if(root.val == 1) return true;
-        boolean l = evaluateTree(root.left);
-        boolean r = evaluateTree(root.right);
-        return root.val==2 ? l|r : l&r;
+if(root.val==1){
+return true;
+}
+        if(root.val==0){
+return false;
+}
+
+            
+            boolean left=evaluateTree(root.left);
+        boolean right=evaluateTree(root.right);
+if(root.val==3){
+return left&& right;
+}
+        else{
+           return left|| right; 
+        }
     }
 }
