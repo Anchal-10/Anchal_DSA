903789e2c988e8b758ddf727f02779e5cd9446be
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-08-20 12:05:19 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (6.33%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -1,44 +1,44 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         if(nums.length==1){
-            return  0;
+            return 0;
         }
-int i=0;
-        int j=nums.length-1;
-        int ans=0;
-        while(i<=j){
-            int mid=i+(j-i)/2;
-                        if(mid>0&& mid<nums.length-1){
-
-            if(nums[mid]>nums[mid+1]&& nums[mid]>nums[mid-1]){
-                ans=mid;
-                break;
-                
-            }
-            else if(nums[mid]<nums[mid+1]){
-                i=mid+1;
-            }
-            else{
-                j=mid-1;
-            }
-        }
-            else if(mid==0){
-                if(nums[mid]>nums[mid+1]){
-                    return 0;
+        int low=0;
+        int high=nums.length-1;
+        int ans=-1;
+        while(low<=high){
+            int mid=(low+high)>>1;
+            if(mid>0&& mid<nums.length-1){
+                if(nums[mid]>nums[mid-1]&& nums[mid]>nums[mid+1]){
+                    ans=mid;
+                    break;
+                }
+                else if(nums[mid+1]>nums[mid]){
+                    low=mid+1;
                 }
                 else{
-return 1;}
+                    high=mid-1;
+}
+            }
+            else if(mid==0){
+                if(nums[1]>nums[0]){
+return 1;
+                }
+                else{
+                    return 0;
+                    
+                }
             }
             else if(mid==nums.length-1){
-                if(nums[mid-1]>nums[mid]){
-                    return mid-1;
+                if(nums[nums.length-1]>nums[nums.length-2]){
+return nums.length-1;
                 }
                 else{
-                    return mid;
+                    return nums.length-2;
+
                 }
             }
         }
         return ans;
     }
 }
-
