6e0e442660b2075dc0e5d28aa68f5f2a523e99d0
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-09-01 02:53:27 +0530

    Time: 1 ms (91.00%), Space: 47.9 MB (58.57%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,26 +1,26 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int target=image[sr][sc];
+int target=image[sr][sc];
         if(target==color){
-return image;
+            return image;
         }
-                   backtrack(image,sr,sc,color,target);
- return image;
+        boolean visited[][]=new boolean[image.length][image[0].length];
+        helper(image,sr,sc,color,target,visited);
+        return image;
     }
+    
+    public void helper(int[][]image,int sr,int sc,int color,int target,boolean visited[][]){
+        if(sc<0|| sr<0|| sr>=image.length|| sc>=image[0].length|| image[sr][sc]!=target|| visited[sr][sc]==true){
+            return;
+            
+        }
+        image[sr][sc]=color;
+        visited[sr][sc]=true;
+        helper(image,sr+1,sc,color,target,visited);
+        helper(image,sr-1,sc,color,target,visited);
+        helper(image,sr,sc-1,color,target,visited);
+        helper(image,sr,sc+1,color,target,visited);
 
         
-        public void backtrack(int a[][],int row,int col,int color,int target){
-            if(row<0||row>=a.length|| col<0|| col>=a[row].length||a[row][col]!=target){
-return;
-            }
-            
-            a[row][col]=color;
-            
-            backtrack(a,row-1,col,color,target);
-            backtrack(a,row+1,col,color,target);
-            backtrack(a,row,col-1,color,target);
-            backtrack(a,row,col+1,color,target);
-
-        }
-    
+    }
 }
