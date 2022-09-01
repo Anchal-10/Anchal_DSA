d99108c6ba3c0a901ed3c1cdf2a2ab37c83afd57
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-09-01 10:04:34 +0530

    Time: 15 ms (25.93%), Space: 62.1 MB (66.54%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.java
+++ 463-island-perimeter/463-island-perimeter.java
@@ -1,38 +1,31 @@
 class Solution {
-    int count=0;
+    int c=0;
     public int islandPerimeter(int[][] grid) {
         for(int i=0;i<grid.length;i++){
-for(int j=0;j<grid[0].length;j++){
-    if(grid[i][j]==1){
-        helper(grid,i,j);
-        break;
-    }
-}}
-        return count;
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1){
+                    helper(grid,i,j,grid.length,grid[0].length);
+                    break;
+                }
+            }
+        }
+        return c;
         
     }
+    public void helper(int[][]a,int i,int j,int m,int n){
+        if(i<0|| j<0||i>=m|| j>=n||a[i][j]==0){
+c++;
+        return;
+        }
+        if(a[i][j]==2){
+            return;
+        }
+        a[i][j]=2;
+        helper(a,i+1,j,m,n);
+       helper(a,i,j+1,m,n);
+        helper(a,i-1,j,m,n);
+        helper(a,i,j-1,m,n);
 
-
-
-public void helper(int [][] a,int i,int j){
-
-
-if(i<0 || i>=a.length || j<0 || j>=a[0].length|| a[i][j]==0){
-    count++;
-    return;
-}
-    if(a[i][j]==-1){
-return;
+        
     }
-    a[i][j]=-1;
-    helper(a,i+1,j);
-    helper(a,i-1,j);
-    helper(a,i,j+1);
-    helper(a,i,j-1);
-
-
-
-
-
-}
 }
