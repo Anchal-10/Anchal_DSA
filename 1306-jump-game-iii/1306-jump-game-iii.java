a0a68641cd244f43b19be3c0193b13c783fe1b26
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-09-01 11:35:51 +0530

    Time: 3 ms (99.34%), Space: 53.4 MB (93.13%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,16 +1,9 @@
 class Solution {
     public boolean canReach(int[] arr, int start) {
-        
-        
-            if(start < 0 || start >= arr.length || arr[start] == -1) return false;
-int v=arr[start];
+        if(start<0 || start>=arr.length || arr[start]<0) return false;
+        if(arr[start] == 0) return true;
 
-        if(arr[start]==0){
-return true;
-        }
-    arr[start]=-1;
-        boolean m= canReach(arr,start+v);
-        boolean n=canReach(arr,start-v);
-      return   m||n;
+        arr[start] = -arr[start];
+        return canReach(arr, start + arr[start]) || canReach(arr, start - arr[start]);
     }
 }
