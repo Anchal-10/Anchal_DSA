d77c45e40594d9aa8355d3bb470f371f8038e6ae
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-09-01 10:46:45 +0530

    Time: 9 ms (85.92%), Space: 51.8 MB (96.60%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
@@ -1,45 +1,45 @@
 class Solution {
+            int max=-1;
+        int max2=-1;
+
     public int[][] findFarmland(int[][] land) {
-        int m=land.length;
+int m=land.length;
         int n=land[0].length;
-List<int[]> temp=new ArrayList<>();
-        int max=0;
-        boolean visited[][]=new boolean[m][n];
+        List<int[]> temp=new ArrayList<>();
+//    List<Integer> al;
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(land[i][j]==1 && visited[i][j]==false){
-                    int arr[]=new int[4];
-                    arr[0]=i;
-                    arr[1]=j;
-                    helper(land,i,j,m,n,visited,arr);
-                    temp.add(arr);
-                }
-            }
+for(int j=0;j<n;j++){
+    if(land[i][j]==1){
+        int arr[]=new int[4];
+        arr[0]=i;
+        arr[1]=j;
+        helper(land,i,j,m,n,arr);
+        temp.add(arr);
+    }
+}
         }
         int res[][]=new int[temp.size()][4];
-        for(int i=0;i<temp.size();i++){
-for(int j=0;j<4;j++){
-    res[i][j]=temp.get(i)[j];
-}
+        for(int i=0;i<res.length;i++){
+            res[i][0]=temp.get(i)[0];
+                        res[i][1]=temp.get(i)[1];
+            res[i][2]=temp.get(i)[2];
+            
+            res[i][3]=temp.get(i)[3];
+
         }
         return res;
-    }
-    
-    public void helper(int [][]a,int i,int j,int m,int n,boolean[][]visited,int[]arr){
-        if(i<0|| j<0|| i>=m|| j>=n || visited[i][j]==true|| a[i][j]==0){
-            return;
-        }
-        arr[2]=Math.max(arr[2],i);
-        arr[3]=Math.max(arr[3],j);
-
-        visited[i][j]=true;
-        helper(a,i+1,j,m,n,visited,arr);
-        helper(a,i,j+1,m,n,visited,arr);
         
-    }
+        
 }
-
-
-
-
+public void helper(int [][]a,int i,int j,int m,int n,int arr[]){
+    if(i<0||j<0|| i>=m|| j>=n|| a[i][j]==0){
+        return;
+    }
+    arr[2]=Math.max(arr[2],i);
+        arr[3]=Math.max(arr[3],j);
+    a[i][j]=0;
+    helper(a,i+1,j,m,n,arr);
+    helper(a,i,j+1,m,n,arr);
+}
+}
 
