0a6c001c5dd5adc3c0c881097b336b8cd4e654bc
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-09-01 10:14:09 +0530

    Time: 3 ms (84.42%), Space: 47.1 MB (71.03%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,32 +1,34 @@
 class Solution {
+    int c=0;
     public int maxAreaOfIsland(int[][] grid) {
-        int m=grid.length;
+            int max=0;
+
+int m=grid.length;
         int n=grid[0].length;
-        int max=0;
-        boolean visited[][]=new boolean[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1 && visited[i][j]==false){
-                    List<Integer> al=new ArrayList<>();
-                    helper(grid,i,j,m,n,visited,al);
-                    max=Math.max(al.size(),max);
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1){
+                    helper(grid,i,j,m,n);
+                    max=Math.max(c,max);
+                    c=0;
                 }
             }
         }
         return max;
+        
     }
-    
-    public void helper(int [][]a,int i,int j,int m,int n,boolean[][]visited,List<Integer> al){
-        if(i<0|| j<0|| i>=m|| j>=n || visited[i][j]==true|| a[i][j]==0){
-            return;
-        }
-        al.add(a[i][j]);
-        visited[i][j]=true;
-        helper(a,i+1,j,m,n,visited,al);
-        helper(a,i-1,j,m,n,visited,al);
-        helper(a,i,j-1,m,n,visited,al);
-        helper(a,i,j+1,m,n,visited,al);
+    public void helper(int a[][],int i,int j,int m,int n){
+if(i<0||j<0|| i>=m|| j>=n|| a[i][j]==0){
+    return;
+}
+    c++;
+        a[i][j]=0;
+        helper(a,i+1,j,m,n);
+      helper(a,i-1,j,m,n);
+        helper(a,i,j+1,m,n);
+        helper(a,i,j-1,m,n);
 
+        
         
     }
 }
