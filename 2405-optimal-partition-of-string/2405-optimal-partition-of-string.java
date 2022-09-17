663c5834a10c7a53910f1b8ebb73bad6257fd628
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-09-17 19:52:20 +0530

    Time: 113 ms (11.42%), Space: 60.7 MB (60.07%) - LeetHub

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int partitionString(String s) {
+Set<Character> set=new HashSet<>();
+        int ans=0;
+    for(int i=0;i<s.length();i++){
+        if(!set.contains(s.charAt(i))){
+            set.add(s.charAt(i));
+        }
+        else{
+            ans++;
+            set.clear();
+            set.add(s.charAt(i));
+        }
+    }
+    
+    return ans+1;
+}
+}
