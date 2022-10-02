1ffbc787601f94844d68e74ed2a07e446f70771e
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-10-02 20:27:58 +0530

    Time: 30 ms (59.46%), Space: 59.8 MB (53.82%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,40 +1,32 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-        if(grid[0][0]==1|| grid[m-1][n-1]==1){
+Queue<int[]> queue=new LinkedList<>();
+    if(grid[0][0]==1){
 return -1;
-        }
-        Queue<int[]> queue=new LinkedList<>();
-        
-queue.offer(new int[]{0,0,1});
-        //break;
-    
+    }
     int dir[][] = new int[][]{{0,1},{0,-1},{1,0},{-1,0},{1,-1},{-1,1},{-1,-1},{1,1}};
 
-    int ans=-1;
+    queue.offer(new int[]{0,0,1});
+       // int dist[][]={{}}
         while(!queue.isEmpty()){
-            int size=queue.size();
-            for(int i=0;i<size;i++){
-                int []temp=queue.poll();
-                if(temp[0]==m-1&& temp[1]==n-1){
-ans= temp[2];
-                    break;
-                }
-                for(int []d: dir){
-                    int nr=d[0]+ temp[0];
-                                        int nc=d[1]+ temp[1];
-                    if(nr>=0&&nr<m&& nc>=0&& nc<n&& grid[nr][nc]==0){
-                        queue.offer(new int[]{nr,nc,temp[2]+1});
-                                            grid[nr][nc]=1;
-
-                    }
-
-                }
-                
+            int[]temp=queue.poll();
+            int m=temp[0];
+            int n=temp[1];
+            int v=temp[2];
+            if(m==grid.length-1&& n==grid[0].length-1){
+                return v;
             }
-                
+            for(int [] d: dir){
+                int newr=d[0]+temp[0];
+                                int newc=d[1]+temp[1];
+                if(newr>=0&& newr<grid.length&& newc>=0&& newc<grid[0].length&& grid[newr][newc]==0){
+                    grid[newr][newc]=1;
+                          queue.offer(new int[]{newr,newc,temp[2]+1});
+                    
+                }
+
             }
-        return ans;
         }
+        return -1;
     }
+}
