7dff1d2f422b628aa61952c31ff96db374f06504
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-10-22 23:04:29 +0530

    Time: 85 ms (84.74%), Space: 39.5 MB (98.01%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -1,28 +1,31 @@
 class Solution {
     public int getMaximumGold(int[][] grid) {
         int max=0;
-        boolean[][]visited=new boolean[grid.length][grid[0].length];
+        boolean[][]vis=new boolean[grid.length][grid[0].length];
 for(int i=0;i<grid.length;i++){
     for(int j=0;j<grid[0].length;j++){
-        max=Math.max(max,helper(i,j,grid,visited));
-
+        if(grid[i][j]!=0){
+            max=Math.max(helper(i,j,grid,vis),max);
+        }
         
     }
 }
+                         
         return max;
     }
-    public int helper(int i,int j,int[][]grid,boolean[][]visited){
-                if(i<0 || j<0 || i >= grid.length || j >= grid[0].length || visited[i][j] || grid[i][j] == 0)
-            return 0; //if overflows like index in -ve or greater than length or is already visited or 0 return 0
-
-        visited[i][j]=true;
-        int left=helper(i-1,j,grid,visited);
-        int right=helper(i+1,j,grid,visited);
-        int up=helper(i,j+1,grid,visited);
-        int down=helper(i,j-1,grid,visited);
-
-        visited[i][j]=false;
-        return Math.max(right,Math.max(left,Math.max(up,down)))+ grid[i][j];
-        
-    }
+    public int helper(int i,int j,int[][]grid,boolean[][]vis){
+if(i<0 || j<0|| i>=grid.length|| j>=grid[0].length|| grid[i][j]==0|| vis[i][j]==true){
+return 0;
 }
+
+
+        vis[i][j]=true;
+        int left=helper(i-1,j,grid,vis);
+                int right=helper(i+1,j,grid,vis); 
+        int down=helper(i,j+1,grid,vis);      
+        int up=helper(i,j-1,grid,vis);
+        vis[i][j]=false;
+        return Math.max(up,Math.max(left,Math.max(right,down)))+grid[i][j];
+    }
+                         }
+                         
