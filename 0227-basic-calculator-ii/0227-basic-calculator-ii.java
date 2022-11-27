29de60a9c1c01edc3c17ef2c17d654726478f2c2
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2022-11-27 23:41:07 +0900

    Time: 18 ms (79.43%), Space: 42.6 MB (87.47%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,37 +1,38 @@
 class Solution {
     public int calculate(String s) {
-        int currNo = 0;
-        char op = '+';
-        int len = s.length();
-        Stack<Integer> stack = new Stack<>();
-        
-        for(int i=0;i<len;i++){
-            char ch = s.charAt(i);
-            
-            //check for digit
+char op='+';
+        Stack<Integer> stack=new Stack<>();
+        int currn=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
             if(Character.isDigit(ch)){
-                currNo = (currNo * 10) + (ch - '0');
+                currn=(currn*10)+(ch-'0');
             }
-            
-            //check for operator
-            if(!Character.isDigit(ch) && ch != ' ' || i == len-1){
-                if(op == '+'){
-                    stack.push(currNo);
-                }else if(op == '-'){
-                    stack.push(-currNo);
-                }else if(op == '*'){
-                    stack.push(stack.pop()*currNo);
-                }else if(op == '/'){
-                    stack.push(stack.pop()/currNo);
+            if((!Character.isDigit(ch) && ch!=' ') || i==s.length()-1){
+                if(op=='+'){
+                    stack.push(currn);
                 }
-                currNo = 0;
-                op = ch;
+                else if(op=='-'){
+                    stack.push(-currn);
+                }
+                else if(op=='*'){
+                    int m=stack.pop();
+                    stack.push(m*currn);
+                }
+                                else if(op=='/'){
+                    int m=stack.pop();
+                    stack.push(m/currn);
+                }
+                op=ch;
+                currn=0;
+
+                
             }
         }
-        int sum = 0;
+        int res=0;
         while(!stack.isEmpty()){
-            sum += stack.pop();
+res+=stack.pop();
         }
-        return sum;
+        return res;
     }
 }
