ca47ca111fcfc84d192355c6fdccbb9c9fb390aa
Anchal-10 <89575859+Anchal-10@users.noreply.github.com>
2024-07-04 00:24:41 +0530

    Time: 1 ms (100.00%), Space: 42.2 MB (25.28%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,21 +1,26 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        return helper(0,0,s,p);
+            Boolean m[][]=new Boolean[s.length()+1][p.length()+1];
+
+        return helper(0,0,s,p,m);
     }
-    public boolean helper(int i, int j, String s, String p){
+    public boolean helper(int i, int j, String s, String p,  Boolean m[][]){
         if(i>=s.length() && j>=p.length()){
 return true;
         }
         if(j>=p.length()){
 return false;
         }
+        if(m[i][j]!=null){
+return m[i][j];
+        }
         
         boolean match = (i<s.length() )&& (p.charAt(j)==s.charAt(i) || (p.charAt(j)=='.'));
         if((j+1)<p.length() && p.charAt(j+1)=='*'){
-return helper(i,j+2,s,p)|| (match &&helper(i+1,j,s,p));
+return m[i][j]=helper(i,j+2,s,p,m)|| (match &&helper(i+1,j,s,p,m));
         }
         if(match){
-            return helper(i+1,j+1,s,p);
+            return m[i][j]= helper(i+1,j+1,s,p,m);
 
         }
         return false;
